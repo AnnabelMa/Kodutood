fca50a0e38bc6966e7142867b0abb53ddad701cb
Annabel Matkur <anmatk@ttu.ee>
2020-02-16 14:55:45 +0200

    max lenght

--- Sport/Models/Sportlane.cs
+++ Sport/Models/Sportlane.cs
@@ -7,7 +7,9 @@ namespace Sport.Models
     public class Sportlane
     {
         public int ID { get; set; }
+        [StringLength(50)]
         public string Perekonnanimi { get; set; }
+        [StringLength(50)]
         public string Eesnimi { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
