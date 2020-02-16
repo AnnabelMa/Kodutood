c29861172b0782c9665546dce4aaf2b3141b6d94
Annabel Matkur <anmatk@ttu.ee>
2020-02-16 15:30:00 +0200

    spordialaAssignment.cs

--- Sport/Models/SpordialaAssignment.cs
+++ Sport/Models/SpordialaAssignment.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sport.Models
 {
-    public class Registreering
+    public class SpordialaAssignment
     {
-        public int RegistreeringID { get; set; }
+        public int TreenerID { get; set; }
         public int SpordialaID { get; set; }
-        public int SportlaneID { get; set; }
-       // [DisplayFormat(NullDisplayText = "No grade")] //KAS ON MINU NÄITEL VAJALIK?
-       
+        public Treener Treener { get; set; }
+
         public Spordiala Spordiala { get; set; }
-        public Sportlane Sportlane { get; set; }
     }
 }
