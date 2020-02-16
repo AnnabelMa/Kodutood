7dec0a8ec253e278b39b75dfe4e36a27934c6d44
Annabel Matkur <anmatk@ttu.ee>
2020-02-16 15:22:13 +0200

    osakond.cs

---
+++ Sport/Models/Osakond.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sport.Models
+{
+    public class Osakond
+    {
+        public int OsakondID { get; set; }
+
+        [StringLength(50, MinimumLength =3)]
+        public string Nimi { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Eelarve { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Alhuse KPe")]
+        public DateTime AlgusKP { get; set; }
+
+        public int? TreenerID { get; set; }
+
+        public Treener Administrator { get; set; }
+        public ICollection<Spordiala> Spordialas { get; set; }
+    }
+}
+
--- Sport/Models/Spordiala.cs
+++ Sport/Models/Spordiala.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sport.Models
@@ -9,10 +10,20 @@ namespace Sport.Models
     public class Spordiala
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Number")]
         public int SpordialaID { get; set; }
+
+        [StringLength(50, MinimumLength =3)]
         public string Nimi { get; set; }
+
+        [Range(0,5)]
         public int Tulemused { get; set; }
 
+        public int OsakondID { get; set; }
+
+        public Osakond Osakond { get; set; }
+
         public ICollection<Registreering> Registreeringud { get; set; }
+        public ICollection<SpordialaAssignment> SpordialaAssignments { get; set; }
     }
 }
