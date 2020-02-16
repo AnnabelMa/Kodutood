7962bfccd1935ba85c520623da26e551cc61b1e8
Annabel Matkur <anmatk@ttu.ee>
2020-02-16 15:11:51 +0200

    AsutuseAssignment.cs

---
+++ Sport/Models/AsutuseAssignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sport.Models
+{
+    public class AsutuseAssignment
+    {
+        [Key]
+        public int TreenerID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Asutuse lokatsioon")]
+        public string Location { get; set; }
+
+        public Treener Treener { get; set; }
+    }
+}
