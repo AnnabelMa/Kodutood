96d81fb4826fd5ffb52568ab687a4a9413c64e09
Annabel Matkur <anmatk@ttu.ee>
2020-02-16 15:41:58 +0200

    dbinitializer updated

--- Sport/Data/DbInitializer.cs
+++ Sport/Data/DbInitializer.cs
@@ -1,6 +1,9 @@
-﻿using Sport.Models;
+﻿
 using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Sport.Models;
 
 namespace Sport.Data
 {
@@ -8,9 +11,9 @@ namespace Sport.Data
     {
         public static void Initialize(SpordiContext context)
         {
-            context.Database.EnsureCreated();
+            //context.Database.EnsureCreated();
 
-            // Look for any students.
+            // Look for any Sportlased.
             if (context.Sportlased.Any())
             {
                 return;   // DB has been seeded
@@ -18,55 +21,232 @@ namespace Sport.Data
 
             var sportlased = new Sportlane[]
             {
-            new Sportlane{Eesnimi="Carson",Perekonnanimi="Alexander",RegistreeringuKP=DateTime.Parse("2005-09-01")},
-            new Sportlane{Eesnimi="Meredith",Perekonnanimi="Alonso",RegistreeringuKP=DateTime.Parse("2002-09-01")},
-            new Sportlane{Eesnimi="Arturo",Perekonnanimi="Anand",RegistreeringuKP=DateTime.Parse("2003-09-01")},
-            new Sportlane{Eesnimi="Gytis",Perekonnanimi="Barzdukas",RegistreeringuKP=DateTime.Parse("2002-09-01")},
-            new Sportlane{Eesnimi="Yan",Perekonnanimi="Li",RegistreeringuKP=DateTime.Parse("2002-09-01")},
-            new Sportlane{Eesnimi="Peggy",Perekonnanimi="Justice",RegistreeringuKP=DateTime.Parse("2001-09-01")},
-            new Sportlane{Eesnimi="Laura",Perekonnanimi="Norman",RegistreeringuKP=DateTime.Parse("2003-09-01")},
-            new Sportlane{Eesnimi="Nino",Perekonnanimi="Olivetto",RegistreeringuKP=DateTime.Parse("2005-09-01")}
+                new Sportlane { Eesnimi = "Carson",   Perekonnanimi = "Alexander",
+                    RegistreeringuKP = DateTime.Parse("2010-09-01") },
+                new Sportlane { Eesnimi = "Meredith", Perekonnanimi = "Alonso",
+                    RegistreeringuKP = DateTime.Parse("2012-09-01") },
+                new Sportlane { Eesnimi = "Arturo",   Perekonnanimi = "Anand",
+                    RegistreeringuKP = DateTime.Parse("2013-09-01") },
+                new Sportlane { Eesnimi = "Gytis",    Perekonnanimi = "Barzdukas",
+                    RegistreeringuKP = DateTime.Parse("2012-09-01") },
+                new Sportlane { Eesnimi = "Yan",      Perekonnanimi = "Li",
+                    RegistreeringuKP = DateTime.Parse("2012-09-01") },
+                new Sportlane { Eesnimi = "Peggy",    Perekonnanimi = "Justice",
+                    RegistreeringuKP = DateTime.Parse("2011-09-01") },
+                new Sportlane { Eesnimi = "Laura",    Perekonnanimi = "Norman",
+                    RegistreeringuKP = DateTime.Parse("2013-09-01") },
+                new Sportlane { Eesnimi = "Nino",     Perekonnanimi = "Olivetto",
+                    RegistreeringuKP = DateTime.Parse("2005-09-01") }
             };
+
             foreach (Sportlane s in sportlased)
             {
                 context.Sportlased.Add(s);
             }
             context.SaveChanges();
 
+            var treenerid = new Treener[]
+            {
+                new Treener { Eesnimi = "Kim",     Perekonnanimi = "Abercrombie",
+                    PalkamiseKP = DateTime.Parse("1995-03-11") },
+                new Treener { Eesnimi = "Fadi",    Perekonnanimi = "Fakhouri",
+                    PalkamiseKP = DateTime.Parse("2002-07-06") },
+                new Treener { Eesnimi = "Roger",   Perekonnanimi = "Harui",
+                    PalkamiseKP = DateTime.Parse("1998-07-01") },
+                new Treener { Eesnimi = "Candace", Perekonnanimi = "Kapoor",
+                    PalkamiseKP = DateTime.Parse("2001-01-15") },
+                new Treener { Eesnimi = "Roger",   Perekonnanimi = "Zheng",
+                    PalkamiseKP = DateTime.Parse("2004-02-12") }
+            };
+
+            foreach (Treener i in treenerid)
+            {
+                context.Treeners.Add(i);
+            }
+            context.SaveChanges();
+
+            var osakonds = new Osakond[]
+            {
+                new Osakond { Nimi = "Motosport",     Eelarve = 350000,
+                    AlgusKP = DateTime.Parse("2007-09-01"),
+                    TreenerID  = treenerid.Single( i => i.Perekonnanimi == "Abercrombie").ID },
+                new Osakond { Nimi = "Veesport", Eelarve = 100000,
+                    AlgusKP = DateTime.Parse("2007-09-01"),
+                    TreenerID  = treenerid.Single( i => i.Perekonnanimi == "Fakhouri").ID },
+                new Osakond { Nimi = "Talisport", Eelarve = 350000,
+                    AlgusKP = DateTime.Parse("2007-09-01"),
+                    TreenerID  = treenerid.Single( i => i.Perekonnanimi == "Harui").ID },
+                new Osakond { Nimi = "Suvesport",   Eelarve = 100000,
+                    AlgusKP = DateTime.Parse("2007-09-01"),
+                    TreenerID  = treenerid.Single( i => i.Perekonnanimi == "Kapoor").ID }
+            };
+
+            foreach (Osakond d in osakonds)
+            {
+                context.Osakonds.Add(d);
+            }
+            context.SaveChanges();
+
             var spordialad = new Spordiala[]
             {
-            new Spordiala{SpordialaID=1050,Nimi="Kergejõustik"},
-            new Spordiala{SpordialaID=4022,Nimi="Ujumine"},
-            new Spordiala{SpordialaID=4041,Nimi="Maadlus"},
-            new Spordiala{SpordialaID=1045,Nimi="Võimlemine"},
-            new Spordiala{SpordialaID=3141,Nimi="Uisutamine"},
-            new Spordiala{SpordialaID=2021,Nimi="Tennis"},
-            new Spordiala{SpordialaID=2042,Nimi="Raskejõustik"}
+                new Spordiala {SpordialaID = 1050, Nimi = "Kergejõustik",
+                    OsakondID = osakonds.Single( s => s.Nimi == "Suvesport").OsakondID
+                },
+                new Spordiala {SpordialaID = 4022, Nimi = "Suusatamine",
+                    OsakondID = osakonds.Single( s => s.Nimi == "Talisport").OsakondID
+                },
+                new Spordiala {SpordialaID = 4041, Nimi = "Autoralli",
+                    OsakondID = osakonds.Single( s => s.Nimi == "Motosport").OsakondID
+                },
+                new Spordiala {SpordialaID = 1045, Nimi = "Uisutamine",
+                    OsakondID = osakonds.Single( s => s.Nimi == "Talisport").OsakondID
+                },
+                new Spordiala {SpordialaID = 3141, Nimi = "Vettehüpped",
+                    OsakondID = osakonds.Single( s => s.Nimi == "Veesport").OsakondID
+                },
+                new Spordiala {SpordialaID = 2021, Nimi = "Laskesuusatamine",
+                    OsakondID = osakonds.Single( s => s.Nimi == "Talisport").OsakondID
+                },
+                new Spordiala {SpordialaID = 2042, Nimi = "Purjetamine",
+                    OsakondID = osakonds.Single( s => s.Nimi == "Suvesport").OsakondID
+                },
             };
+
             foreach (Spordiala c in spordialad)
             {
                 context.Spordiala.Add(c);
             }
             context.SaveChanges();
 
-            var registreeringud = new Registreering[]
+            var asutuseAssignments = new AsutuseAssignment[]
             {
-            new Registreering{SportlaseID=1,SpordialaID=1050},
-            new Registreering{SportlaseID=1,SpordialaID=4022},
-            new Registreering{SportlaseID=1,SpordialaID=4041},
-            new Registreering{SportlaseID=2,SpordialaID=1045},
-            new Registreering{SportlaseID=2,SpordialaID=3141},
-            new Registreering{SportlaseID=2,SpordialaID=2021},
-            new Registreering{SportlaseID=3,SpordialaID=1050},
-            new Registreering{SportlaseID=4,SpordialaID=1050},
-            new Registreering{SportlaseID=4,SpordialaID=4022},
-            new Registreering{SportlaseID=5,SpordialaID=4041},
-            new Registreering{SportlaseID=6,SpordialaID=1045},
-            new Registreering{SportlaseID=7,SpordialaID=3141},
+                new AsutuseAssignment {
+                    TreenerID = treenerid.Single( i => i.Perekonnanimi == "Fakhouri").ID,
+                    Location = "Smith 17" },
+                new AsutuseAssignment {
+                    TreenerID = treenerid.Single( i => i.Perekonnanimi == "Harui").ID,
+                    Location = "Gowan 27" },
+                new AsutuseAssignment {
+                    TreenerID = treenerid.Single( i => i.Perekonnanimi == "Kapoor").ID,
+                    Location = "Thompson 304" },
             };
-            foreach (Registreering r in registreeringud)
+
+            foreach (AsutuseAssignment o in asutuseAssignments)
             {
-                context.Registreeringud.Add(r);
+                context.AsutuseAssignments.Add(o);
+            }
+            context.SaveChanges();
+
+            var spordialatreenerid = new SpordialaAssignment[]
+            {
+                new SpordialaAssignment {
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Kergejõustik" ).SpordialaID,
+                    TreenerID = treenerid.Single(i => i.Perekonnanimi == "Kapoor").ID
+                    },
+                new SpordialaAssignment {
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Kergejõustik" ).SpordialaID,
+                    TreenerID = treenerid.Single(i => i.Perekonnanimi == "Harui").ID
+                    },
+                new SpordialaAssignment {
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Suusatamine" ).SpordialaID,
+                    TreenerID = treenerid.Single(i => i.Perekonnanimi == "Zheng").ID
+                    },
+                new SpordialaAssignment {
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Autoralli" ).SpordialaID,
+                    TreenerID = treenerid.Single(i => i.Perekonnanimi == "Zheng").ID
+                    },
+                new SpordialaAssignment {
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Uisutamine" ).SpordialaID,
+                    TreenerID = treenerid.Single(i => i.Perekonnanimi == "Fakhouri").ID
+                    },
+                new SpordialaAssignment {
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Vettehüpped" ).SpordialaID,
+                    TreenerID = treenerid.Single(i => i.Perekonnanimi == "Harui").ID
+                    },
+                new SpordialaAssignment {
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Laskesuusatamine" ).SpordialaID,
+                    TreenerID = treenerid.Single(i => i.Perekonnanimi == "Abercrombie").ID
+                    },
+                new SpordialaAssignment {
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Purjetamine" ).SpordialaID,
+                    TreenerID = treenerid.Single(i => i.Perekonnanimi == "Abercrombie").ID
+                    },
+            };
+
+            foreach (SpordialaAssignment si in spordialatreenerid)
+            {
+                context.SpordialaAssignments.Add(si);
+            }
+            context.SaveChanges();
+
+            var registreering = new Registreering[]
+            {
+                new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Alexander").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Kergejõustik" ).SpordialaID,
+
+                },
+                    new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Alexander").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Suusatamine" ).SpordialaID,
+
+                    },
+                    new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Alexander").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Purjetamine" ).SpordialaID,
+
+                    },
+                    new Registreering {
+                        SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Alonso").ID,
+                    SpordialaID =  spordialad.Single(c => c.Nimi == "Vettehüpped" ).SpordialaID,
+
+                    },
+                    new Registreering {
+                        SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Alonso").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Uisutamine" ).SpordialaID,
+
+                    },
+                    new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Alonso").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Laskesuusatamine" ).SpordialaID,
+
+                    },
+                    new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Anand").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Kergejõustik" ).SpordialaID
+                    },
+                    new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Anand").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Autoralli").SpordialaID,
+
+                    },
+                new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Barzdukas").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Kergejõustik").SpordialaID,
+
+                    },
+                    new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Li").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Autoralli").SpordialaID,
+
+                    },
+                    new Registreering {
+                    SportlaneID = sportlased.Single(s => s.Perekonnanimi == "Justice").ID,
+                    SpordialaID = spordialad.Single(c => c.Nimi == "Vettehüpped").SpordialaID,
+
+                    }
+            };
+
+            foreach (Registreering r in registreering)
+            {
+                var RegistreeringInDataBase = context.Registreeringud.Where(
+                    s =>
+                            s.Sportlane.ID == r.SportlaneID &&
+                            s.Spordiala.SpordialaID == r.SpordialaID).SingleOrDefault();
+                if (RegistreeringInDataBase == null)
+                {
+                    context.Registreeringud.Add(r);
+                }
             }
             context.SaveChanges();
         }
---
+++ Sport/Migrations/20200216133859_ComplexDataModel.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sport.Migrations
+{
+    public partial class ComplexDataModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registreering_Sportlane_SportlaneID",
+                table: "Registreering");
+
+            migrationBuilder.DropColumn(
+                name: "SportlaseID",
+                table: "Registreering");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Perekonnanimi",
+                table: "Sportlane",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nimi",
+                table: "Spordiala",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+           /* migrationBuilder.AddColumn<int>(
+                name: "OsakondID",
+                table: "Spordiala",
+                nullable: false,
+                defaultValue: 0);
+           */
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SportlaneID",
+                table: "Registreering",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Treener",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Perekonnanimi = table.Column<string>(maxLength: 50, nullable: false),
+                    Eesnimi = table.Column<string>(maxLength: 50, nullable: false),
+                    PalkamiseKP = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treener", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AsutuseAssignment",
+                columns: table => new
+                {
+                    TreenerID = table.Column<int>(nullable: false),
+                    Location = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AsutuseAssignment", x => x.TreenerID);
+                    table.ForeignKey(
+                        name: "FK_AsutuseAssignment_Treener_TreenerID",
+                        column: x => x.TreenerID,
+                        principalTable: "Treener",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Osakond",
+                columns: table => new
+                {
+                    OsakondID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nimi = table.Column<string>(maxLength: 50, nullable: true),
+                    Eelarve = table.Column<decimal>(type: "money", nullable: false),
+                    AlgusKP = table.Column<DateTime>(nullable: false),
+                    TreenerID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Osakond", x => x.OsakondID);
+                    table.ForeignKey(
+                        name: "FK_Osakond_Treener_TreenerID",
+                        column: x => x.TreenerID,
+                        principalTable: "Treener",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.Sql("INSERT INTO dbo.Department (Name, Budget, StartDate) VALUES ('Temp', 0.00, GETDATE())");
+            // Default value for FK points to department created above, with
+            // defaultValue changed to 1 in following AddColumn statement.
+
+            migrationBuilder.AddColumn<int>(
+                name: "OsakondID",
+                table: "Spordiala",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.CreateTable(
+                name: "SpordialaAssignment",
+                columns: table => new
+                {
+                    TreenerID = table.Column<int>(nullable: false),
+                    SpordialaID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpordialaAssignment", x => new { x.SpordialaID, x.TreenerID });
+                    table.ForeignKey(
+                        name: "FK_SpordialaAssignment_Spordiala_SpordialaID",
+                        column: x => x.SpordialaID,
+                        principalTable: "Spordiala",
+                        principalColumn: "SpordialaID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SpordialaAssignment_Treener_TreenerID",
+                        column: x => x.TreenerID,
+                        principalTable: "Treener",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Spordiala_OsakondID",
+                table: "Spordiala",
+                column: "OsakondID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Osakond_TreenerID",
+                table: "Osakond",
+                column: "TreenerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpordialaAssignment_TreenerID",
+                table: "SpordialaAssignment",
+                column: "TreenerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registreering_Sportlane_SportlaneID",
+                table: "Registreering",
+                column: "SportlaneID",
+                principalTable: "Sportlane",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Spordiala_Osakond_OsakondID",
+                table: "Spordiala",
+                column: "OsakondID",
+                principalTable: "Osakond",
+                principalColumn: "OsakondID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registreering_Sportlane_SportlaneID",
+                table: "Registreering");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Spordiala_Osakond_OsakondID",
+                table: "Spordiala");
+
+            migrationBuilder.DropTable(
+                name: "AsutuseAssignment");
+
+            migrationBuilder.DropTable(
+                name: "Osakond");
+
+            migrationBuilder.DropTable(
+                name: "SpordialaAssignment");
+
+            migrationBuilder.DropTable(
+                name: "Treener");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Spordiala_OsakondID",
+                table: "Spordiala");
+
+            migrationBuilder.DropColumn(
+                name: "OsakondID",
+                table: "Spordiala");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Perekonnanimi",
+                table: "Sportlane",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nimi",
+                table: "Spordiala",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SportlaneID",
+                table: "Registreering",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "SportlaseID",
+                table: "Registreering",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registreering_Sportlane_SportlaneID",
+                table: "Registreering",
+                column: "SportlaneID",
+                principalTable: "Sportlane",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
