8d43a770b20e7f29d694a8cc17cf959e4e8721be
Annabel Matkur <anmatk@ttu.ee>
2020-02-17 16:55:35 +0200

    sportlane : person treener : person pärimised

--- Sport/Models/Sportlane.cs
+++ Sport/Models/Sportlane.cs
@@ -5,29 +5,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sport.Models
 {
-    public class Sportlane
+    public class Sportlane : Person
     {
-        public int ID { get; set; }
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "Perekonnanimi")]
-        public string Perekonnanimi { get; set; }
-        [StringLength(50)]
-        [Column("Eesnimi")]
-        [Display(Name = "Eesnimi")]
-        public string Eesnimi { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Registreerimise kuupäev")]
+        [Display(Name = "RegistreeringuKP")]
         public DateTime RegistreeringuKP { get; set; }
-        [Display(Name = "Täisnimi")]
-        public string Täisnimi
-        {
-            get
-            {
-                return Perekonnanimi + ", " + Eesnimi;
-            }
-        }
+
+
         public ICollection<Registreering> Registreeringud { get; set; }
     }
 }
--- Sport/Models/Treener.cs
+++ Sport/Models/Treener.cs
@@ -7,31 +7,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sport.Models
 {
-    public class Treener
+    public class Treener : Person
     {
-        public int ID { get; set; }
-
-        [Required]
-        [Display(Name = "Perekonnanimi")]
-        [StringLength(50)]
-        public string Perekonnanimi { get; set; }
-
-        [Required]
-        [Column("Eesnimi")]
-        [Display(Name = "Eesnimi")]
-        [StringLength(50)]
-        public string Eesnimi { get; set; }
-
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Palkamise KP")]
+        [Display(Name = "PalkamiseKP")]
         public DateTime PalkamiseKP { get; set; }
-
-        [Display(Name = "Täisnimi")]
-        public string Täisnimi
-        {
-            get { return Perekonnanimi + ", " + Eesnimi; }
-        }
 
         public ICollection<SpordialaAssignment> SpordialaAssignments { get; set; }
         public AsutuseAssignment AsutuseAssignment { get; set; }
