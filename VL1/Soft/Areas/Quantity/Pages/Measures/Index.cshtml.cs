5eb1f29a648871b3ef2123566a0abb13147e24c1
Annabel Matkur <anmatk@ttu.ee>
2020-02-27 09:37:36 +0200

    previous ja next nupud

--- VL1/Soft/Areas/Quantity/Pages/Measures/Index.cshtml.cs
+++ VL1/Soft/Areas/Quantity/Pages/Measures/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Facade.Quantity;
 using VL1.Domain.Quantity;
@@ -9,12 +8,11 @@ namespace VL1.Soft.Areas.Quantity.Pages.Measures
 {
     public class IndexModel : MeasuresPage
     {
-        public string SearchString;
         public IndexModel(IMeasuresRepository r) : base(r) { }
 
         public async Task OnGetAsync(string sortOrder, string searchString) 
         {
-            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
             db.SortOrder = sortOrder;
             SearchString = searchString;
@@ -23,7 +21,16 @@ namespace VL1.Soft.Areas.Quantity.Pages.Measures
             Items = new List<MeasureView>();
             foreach (var e in l)  Items.Add(MeasureViewFactory.Create(e));
         }
+        public string CurrentSort { get; set; }
         public string DateSort { get; set; }
         public string NameSort { get; set; }
+
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
+        public int PageIndex { get; set; }
+
+        public string CurrentFilter { get; set; }
+        public string SearchString { get; set; }
+
     }
 }
