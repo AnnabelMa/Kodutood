503cf3132300a68bfc1744da6d808e28abcb6dea
Annabel Matkur <anmatk@ttu.ee>
2020-02-29 22:54:48 +0200

    VL15 - vaadete mallid (template); +logi

--- VL1/Pages/Quantity/MeasuresPage.cs
+++ VL1/Pages/Quantity/MeasuresPage.cs
@@ -14,5 +14,11 @@ namespace VL1.Pages.Quantity
         [BindProperty]
         public MeasureView Item { get; set; }
         public IList<MeasureView> Items { get; set; }
+
+        public string PageTitle { get; set; } = "Mingi pealkiri";
+        public string CurrentSort { get; set; } = "Current Sort";
+        public string CurrentFilter { get; set; } = "Current Filter";
+        public int PageIndex { get; set; } = 3;
+        public int TotalPages { get; set; } = 10;
     }
 }
