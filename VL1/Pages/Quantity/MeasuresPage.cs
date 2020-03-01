eefd3adc740cab756d8a8c25ef0f30e672d34a18
Annabel Matkur <anmatk@ttu.ee>
2020-03-01 10:06:12 +0200

    VL16 - template jätk, +logi

--- VL1/Pages/Quantity/MeasuresPage.cs
+++ VL1/Pages/Quantity/MeasuresPage.cs
@@ -9,13 +9,20 @@ namespace VL1.Pages.Quantity
     public abstract class MeasuresPage: PageModel
     {
         protected internal readonly IMeasuresRepository db;
-        protected internal MeasuresPage(IMeasuresRepository r) => db = r;
 
+        protected internal MeasuresPage(IMeasuresRepository r)
+        {
+            db = r;
+            PageTitle = "Measures";
+        }
         [BindProperty]
         public MeasureView Item { get; set; }
         public IList<MeasureView> Items { get; set; }
 
-        public string PageTitle { get; set; } = "Mingi pealkiri";
+        public string ItemId => Item.Id;
+
+        public string PageTitle { get; set; }
+        public string PageSubTitle { get; set; }
         public string CurrentSort { get; set; } = "Current Sort";
         public string CurrentFilter { get; set; } = "Current Filter";
         public int PageIndex { get; set; } = 3;
