797dfcd1592a8c6bbb18ced81025fb71bc2c89a0
Annabel Matkur <anmatk@ttu.ee>
2020-03-02 16:40:02 +0200

    VL 18; SortedRepository abstraktseks; VL 19; SortedRepository vigade parandus; +logi

--- VL1/Infra/SortedRepository.cs
+++ VL1/Infra/SortedRepository.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using VL1.Data.Common;
 using VL1.Domain.Common;
 
@@ -9,9 +13,52 @@ namespace VL1.Infra
         where TDomain : Entity<TData>, new()
     {
         public string SortOrder { get; set; }
+        public string DescendingString => "_desc";
+        protected SortedRepository(DbContext c, DbSet<TData> s) : base(c, s) { }
 
-        protected SortedRepository(DbContext c, DbSet<TData> s) : base(c, s)
+        //kirjutab lause, milles on SQL sorteerimine sees:
+        protected internal IQueryable<TData> setSorting(IQueryable<TData> data)
         {
+            var expression = createExpression();
+            if (expression is null) return data;
+            return setOrderBy(data, expression);
         }
+
+        internal Expression<Func<TData, object>> createExpression()
+        {
+            var property = findProperty();
+            if (property is null) return null;
+            return lambdaExpression(property);
+        }
+
+        internal Expression<Func<TData, object>> lambdaExpression(PropertyInfo p)
+        {
+            //1)kõigepealt tuleb öelda ära, millisest tüübist hakkad lambdaExpressionit tegema
+            //2)peab ütlema, et property on parameetrist selle property info peale
+            //3)property on selline, aga konventeeri property tüübiks
+            //ja body on see mida hakkan kasutama
+            //4) kogu body pane käituma nagu lambdaExpression
+            var param = Expression.Parameter(typeof(TData));
+            var property = Expression.Property(param, p);
+            var body = Expression.Convert(property, typeof(object));
+            return Expression.Lambda<Func<TData, object>>(body, param);
+        }
+
+        internal PropertyInfo findProperty()
+        {
+            var name = getName();
+            return typeof(TData).GetProperty(name);
+        }
+        private string getName() //sortOrderist võtab pealt kõik characterid, mis on DescendingStringis
+        {
+            if (string.IsNullOrEmpty(SortOrder)) return string.Empty;
+            var idx = SortOrder.IndexOf(DescendingString, StringComparison.Ordinal);
+            if (idx > 0) return SortOrder.Remove(idx);
+            return SortOrder;
+        }
+        //kui on DescendingString, tuleb sorteerida õigetpidi, kui pole, siis teistpidi.
+        internal IQueryable<TData> setOrderBy(IQueryable<TData> data, Expression<Func<TData, object>> e) 
+            => isDescending() ? data.OrderByDescending(e) : data.OrderBy(e);
+        internal bool isDescending() => SortOrder.EndsWith(DescendingString);
     }
 }
--- VL1/Soft/Areas/Quantity/Pages/Measures/Index.cshtml.cs
+++ VL1/Soft/Areas/Quantity/Pages/Measures/Index.cshtml.cs
@@ -10,10 +10,12 @@ namespace VL1.Soft.Areas.Quantity.Pages.Measures
     {
         public IndexModel(IMeasuresRepository r) : base(r) { }
 
-        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex) 
+        public async Task OnGetAsync(string sortOrder, string currentFilter, 
+            string searchString, int? pageIndex)
         {
+            sortOrder = string.IsNullOrEmpty(sortOrder) ? "Name_desc" : sortOrder;
             CurrentSort = sortOrder;
-            NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            NameSort = sortOrder =="Name" ? "Name_desc" : "Name";
             IdSort = sortOrder == "Id" ? "Id_desc" : "Id";
             CodeSort = sortOrder == "Code" ? "Code_desc" : "Code";
             DefinitionSort = sortOrder == "Definition" ? "Definition_desc" : "Definition";
